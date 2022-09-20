952d9c9495d1bae596fc878950bdd2a177c1702e
inan guclu <77547694+inanguclu@users.noreply.github.com>
2022-09-21 02:01:25 +0300

    service create

---
+++ src/test/java/springecommerceproject2/SpringECommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package springecommerceproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringECommerceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
