194b81147f2531f71cfa06ff7d986532bdc57842
inan guclu <77547694+inanguclu@users.noreply.github.com>
2022-09-21 00:05:06 +0300

    first commit

---
+++ Spring-E-Commerce-Project/src/main/java/com/works/springecommerceproject/SpringECommerceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.works.springecommerceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringECommerceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringECommerceProjectApplication.class, args);
+    }
+
+}
---
+++ Spring-E-Commerce-Project/src/main/java/com/works/springecommerceproject/entities/Role.java
@@ -0,0 +1,27 @@
+package com.works.springecommerceproject.entities;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+
+}
